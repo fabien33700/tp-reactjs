bc7becd902d047f8f95dce26e8d938ef882d3afc
Fabien LH <fabien.lehouedec@gmail.com>
2019-10-04 20:17:25 +0200

    Adding comments and detail about exercice realization

--- README.md
+++ README.md
@@ -5,4 +5,20 @@
 
 **Note :** I used `react-bootstrap` components and `react-icons` for creating a UI as nice as possible.
 
+For the Advanced version, please run `npm start` in `api/` folder to launch the mocked Web service API.
+I did not manage to connect with Parse backend correctly, so to avoid being stucked for the end of the exercise, I developed a tiny REST API Endpoint for managing **users** and **costs**
+
+I succeeded to display the content provided by the WS in the application : 
+  - I set proxy up in package.json so non-static HTTP calls will be forwarded by the dev server to the API endpoint one (localhost:4000)
+  - I create my API endpoint with express and a simple in-memory data model
+
+I didn't succeed to : 
+  - Add new cost. I think I'm near to make this work but it seems my code has became to messy so I'm lost
+
+Please read comments I wrote in the createStore file to have more details about why I was getting stuck.
+
+Thank you, 
+
+Regards
+
 *Fabien Le Houëdec*
--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -21,6 +21,10 @@ export default function configureStore(preloadedState = {}) {
 
     const store = createStore(createRootReducer(), preloadedState, middlewares)
     store.asyncReducers = {};
+
+    // This method is supposed to dispatch async dispatchers
+    // but I don't know where to call it in the component code.
+    // My goal was to make the POST HTTP call and to trigger the UI update
     store.update = () => {
         for (const handler in asyncHandlers) {
             store.dispatch(handler.dispatcher)
@@ -34,8 +38,23 @@ export default function configureStore(preloadedState = {}) {
     return store
 }
 
+/**
+ * Create an asynchronous action handler, which means a couple composed of the promise
+ * dispatcher and the associated reducer
+ *  - dispatcher is responsible for starting the promise resolution and dispatching action corresponding
+ *    to different request resolution state
+ *  - reducer is the handler responsible for managing the state during the promise resolution lifecycle
+ * (I tried to make more abstract the management of asynchronous action handling, it's a try and 
+ * it's probably not a good and used pattern in React)
+ * @param {string} actionType The action prefix used during all the async treatment lifecycle
+ * @param {Promise} promise The promise that will be used to retrieve data synchronously
+ * @param {mixed} defaultValue Default value if promise fail or return null value
+ */
 function createAsyncActionHandler(actionType, promise, defaultValue = {}) {
 
+    /**
+     * The async state wraps the value and trace the state of the promise resolution (pending, fetched, etc.)
+     */
     function asyncState() {
         return {
             pending: false,
@@ -45,10 +64,17 @@ function createAsyncActionHandler(actionType, promise, defaultValue = {}) {
         }
     }
 
+    /**
+     * Execute the promise and dispatch actions according to its state.
+     * Given that reducer and dispatcher is generated in the same scope,
+     *   so it ensures that they manage the same actions.     * 
+     * @param {*} dispatch 
+     */
     function dispatcher(dispatch) {
         dispatch({ type: actionType + '.start' })
         promise
             .then(response =>
+                // Dispatch the action for fulfilled promise
                 dispatch({ type: actionType + '.fulfilled', payload: response }))
             .catch(err =>
                 dispatch({ type: actionType + '.error', payload: err }))
@@ -72,9 +98,11 @@ function createAsyncActionHandler(actionType, promise, defaultValue = {}) {
     }
 
     function reducer(state = asyncState(), action) {
+        // Only treat action with the matching prefix
         if (action.type.substring(0, actionType.length) !== actionType)
             return state
 
+        // Parsing the promise phase from the action name (start, error, fulfilled)
         const phase = action.type.substring(action.type.lastIndexOf(".") + 1)
 
         const actionHandlers = {
@@ -83,6 +111,7 @@ function createAsyncActionHandler(actionType, promise, defaultValue = {}) {
             'fulfilled': () => onFulfilled(state, action.payload)
         }
 
+        // Executing the corresponding handler to create the state, or return the state by default
         return (actionHandlers[phase] || (() => state))()
     }
 
